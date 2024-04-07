921dd71753adfbb4ce0b401e7302d250150a5fef
Karam Abbas <70372614+Karam-Abbas@users.noreply.github.com>
2024-04-07 16:06:41 +0500

    Add files via upload

---
+++ JavaScript/DOM/Dom Practice Tasks/Copyright_helper/style.js
@@ -0,0 +1,5 @@
+let a = document.querySelector(".text");
+
+a.addEventListener("copy", ()=>{
+    a.innerHTML = "<h1>Han bhai sb copy krle source code deta hon beech ke apni company khol le👿👿👿👿👿</h1>";
+});
