cbeb333368733bd948cd589d079f5072665a5a26
Karam Abbas <70372614+Karam-Abbas@users.noreply.github.com>
2024-04-11 20:04:58 +0500

    Add files via upload

---
+++ JavaScript/Projects/5. KeyCode generator/app.js
@@ -0,0 +1,22 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keypress",(event)=>
+{
+  insert.innerHTML=
+  `
+  <div class="key">
+  ${event.key === " "  ?  "Space" : event.key}
+  <small>Key</small>
+  </div>
+
+  <div class="key">
+  ${event.keyCode}
+  <small>Key Code</small>
+  </div>
+
+  <div class="key">
+  ${event.code}
+  <small>Code</small>
+  </div>
+  `;
+})
