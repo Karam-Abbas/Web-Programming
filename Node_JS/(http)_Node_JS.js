809068ebabdb62caea1f82cd0cb7d20998da817a
Karam Abbas <70372614+Karam-Abbas@users.noreply.github.com>
2024-06-22 21:00:14 +0500

    Add files via upload

---
+++ Node_JS/(http)_Node_JS.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+//Server Creation :--
+
+// const server = http.createServer((request,response)=>
+//     {
+//         response.write("Hello");
+//         response.end(" World! ");
+//     })
+//     server.listen(8080);
+
+console.log("Hello babe!")
