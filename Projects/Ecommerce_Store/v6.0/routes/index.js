fd66a342fc8d39108c078e04675f51d3e2610a06
Karam Abbas <70372614+Karam-Abbas@users.noreply.github.com>
2024-07-19 00:32:08 +0500

    Add files via upload

---
+++ Projects/Ecommerce_Store/v6.0/routes/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const isLoggedIn = require("../middlewares/isLoggedIn");
+const productModel = require("../models/productModel");
+const userModel = require("../models/userModel");
+
+router.get("/", function (req, res) {
+  let error = req.flash("error");
+  res.render("index", { error, loggedin: false });
+});
+
+router.get("/shop", isLoggedIn, async function (req, res) {
+  let products = await productModel.find();
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isLoggedIn, async function (req, res) {
+  const user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+  const bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount);
+  res.render("cart", { user, bill });
+});
+
+router.get("/addtocart/:productId", isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productId);
+  await user.save();
+  req.flash("success", "Item added to cart successfully");
+  res.redirect("/shop");
+});
+
+module.exports = router;
