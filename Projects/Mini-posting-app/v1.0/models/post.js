f0223bd42e67d0a8a4173b93db74f3dd4bee6908
Karam Abbas <70372614+Karam-Abbas@users.noreply.github.com>
2024-07-21 20:52:19 +0500

    Delete Projects/Mini-posting-app directory

--- Projects/Mini-posting-app/v1.0/models/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/AppDB");
-
-let postSchema = mongoose.Schema({
-
-});
-
-module.exports = mongoose.model("post",postSchema)
